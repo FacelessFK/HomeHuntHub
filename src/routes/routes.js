3d2f29c167cc955d59e14e73e0ad5b9b785ae4b8
Faceless_fk21 <fardin.k.s.10.1@gmail.com>
2023-09-15 16:58:47 +0330

    add login , logout and some more to routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const Route = express.Router();
-const { createUser } = require("../controller/user");
+const auth = require("../middleware/auth");
+const { createUser, logIn, logOut, authedUser } = require("../controller/user");
 
 Route.route("/api/v1/users").post(createUser);
+Route.route("/api/v1/users/login").post(logIn);
+Route.route("/api/v1/users/logout").post(auth, logOut);
+Route.route("/api/v1/users/me").get(auth, authedUser).patch(auth);
 module.exports = Route;
